8f9bd8845dd7b19150cd557f1463cbc10ed01357
Sepehr Safari <safari.sepehr@gmail.com>
2023-06-03 12:05:20 +0330

    add logout mechanism

--- src/LocalStorage.ts
+++ src/LocalStorage.ts
@@ -1,7 +1,12 @@
 /** abstracted to improve testability */
-export const localStorageGetItem = (key:string):string | null => localStorage.getItem(key);
+export const localStorageGetItem = (key: string): string | null => localStorage.getItem(key);
 
 /** abstracted to improve testability */
-export const localStorageSetItem = (key:string, value:string):void => {
+export const localStorageSetItem = (key: string, value: string): void => {
   localStorage.setItem(key, value);
+};
+
+/** abstracted to improve testability */
+export const localStorageClear = (): void => {
+  localStorage.clear();
 };
